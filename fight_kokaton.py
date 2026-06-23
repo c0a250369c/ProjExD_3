13325bac8da9f1726f47c6a80dfadd80fb01e643
c0a250369c <c0a250369c@edu.teu.ac.jp>
2026-06-23 14:50:52 +0900

    ゲームオーバー画面

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -162,16 +162,21 @@ def main():
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
+                fonto = pg.font.Font(None, 80)
+                txt = fonto.render("Game Over", True, (255, 0, 0))
+                screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
                 pg.display.update()
                 time.sleep(1)
                 return
         
-        if beam is not None:
-            if bomb is not None:
+        if bomb is not None:
+            if beam is not None:
                 if beam.rct.colliderect(bomb.rct):
+                    bird.change_img(6, screen)
+                    pg.display.update()
                     beam = None
-                    beam = None
-                    return
+                    bomb = None
+                    
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
